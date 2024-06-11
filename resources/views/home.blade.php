28184b905756e40432a2dd96caca281272816e58
Alecaring <Private_a.a.c@icloud.com>
2024-06-12 01:13:00 +0200

    add: first half style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,53 @@
 @section('content')
     <div class="cont-main">
         @foreach ($datas as $data)
+
+
+        @php
+        // Creazione degli oggetti DateTime per le date di partenza e arrivo
+        $date_partenza = new DateTime($data->orario_partenza);
+        $formattedTime_partenza = $date_partenza->format('H:i');
+    
+        $date_arrivo = new DateTime($data->orario_arrivo);
+        $formattedTime_arrivo = $date_arrivo->format('H:i');
+    
+        // Calcolo della differenza di tempo tra partenza e arrivo
+        $interval = $date_partenza->diff($date_arrivo);
+    
+        // Formattazione della differenza in ore e minuti
+        $difference = $interval->format('%h ore %i minuti');
+    @endphp
+
+
             <div class="container_cards">
-                <span>{{ $data->id }}</span>
-                <span>{{ $data->azienda }}</span>
-                <span>{{ $data->stazione_partenza }}</span>
-                <span>{{ $data->stazione_arrivo }}</span>
-                <span>{{ $data->orario_partenza }}</span>
-                <span>{{ $data->orario_arrivo }}</span>
-                <span>{{ $data->codeice_treno }}</span>
+                <div class="top_cont">
+
+                    <span class="agg">{{ $data->azienda }}</span>
+                    <span><strong>cod:</strong> {{ $data->codice_treno }}</span>
+
+                </div>
+                <div class="center_cont">
+                    <div class="left_center_cont">
+                        <span class="genral">
+
+
+                            <span class="num">{{ $formattedTime_partenza }}</span>
+                            <span class="arr">{{ $data->stazione_partenza }}</span>
+
+                        </span>
+                        <span class="genral">
+                            <span>•••••••    {{ $difference }}    •••••••</span>
+
+                        </span>
+                        <span class="genral">
+
+                            <span class="num">{{ $formattedTime_arrivo }}</span>
+                            <span class="arr">{{ $data->stazione_arrivo }}</span>
+                        </span>
+                    </div>
+                    <div class="right_center_cont"></div>
+                </div>
+                <div class="bottom_cont"></div>
                 <span>{{ $data->numero_carrozze }}</span>
                 <span>{{ $data->in_orario }}</span>
                 <span>{{ $data->cancellato }}</span>
--- resources/views/partials/Header.blade.php
+++ resources/views/partials/Header.blade.php
@@ -6,9 +6,9 @@
                         <li>Logo Treno</li>
                     </div>
                     <div class="cont_center">
-                        <li><a href="{{ route('home') }}">HOME</a></li>
-                        <li><a href="{{ route('about') }}">ABOUT</a></li>
-                        <li><a href="{{ route('contact') }}">CONTACT</a></li>
+                        <li><a class="{{ Request::route()->getName() === 'home' ? 'active' : '' }}" href="{{ route('home') }}">HOME</a></li>
+                        <li><a class="{{ Request::route()->getName() === 'about' ? 'active' : '' }}" href="{{ route('about') }}">ABOUT</a></li>
+                        <li><a class="{{ Request::route()->getName() === 'contact' ? 'active' : '' }}" href="{{ route('contact') }}">CONTACT</a></li>
                     </div>
                     <div class="cont_right">
                         <li>user</li>
